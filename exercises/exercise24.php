8551c5edc1e40cf926f0424c74b2afcf929a8153
Gabriel Bueno <64340912+lugabrielbueno@users.noreply.github.com>
2020-07-02 20:59:12 -0300

    arrays and constants php

---
+++ exercises/exercise24.php
@@ -0,0 +1,18 @@
+<?php
+
+//constants
+
+/*
+define("BANCO_DE_DADOS", [
+    "127.0.0.1",
+    'root',
+    'pass',
+    'test']);
+
+print_r(BANCO_DE_DADOS);
+*/
+echo PHP_VERSION;
+echo '<br>';
+echo DIRECTORY_SEPARATOR;
+
+?>
