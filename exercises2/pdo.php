78d08d5d98d75135d7b890f062c1213c8eeb2609
Gabriel Bueno <64340912+lugabrielbueno@users.noreply.github.com>
2020-07-22 21:29:34 -0300

    training pdo

--- exercises2/pdo.php
+++ exercises2/pdo.php
@@ -2,14 +2,16 @@
 
 $conn = new PDO("mysql:dbname=book_shop; host=localhost", "root","anarquia99");
 
-$stmt = $conn->prepare("SELECT * FROM books;");
+$stmt = $conn->prepare("SELECT title,author_fname FROM books;");
 
 $stmt->execute();
 
-$results =$stmt->fetchAll(PDO::FETCH_ASSOC);
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-foreach($results as $result) {
-    var_dump($result);
+foreach($results as $res) {
+    foreach($res as $re){
+        var_dump($re);
+    }
 }
 
 ?>
