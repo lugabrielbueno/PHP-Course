2e46f7c4f11c50a1aefebeb0f5c95e0b985a33c0
Gabriel Bueno <64340912+lugabrielbueno@users.noreply.github.com>
2020-07-24 20:12:19 -0300

    cadastro de livros integrado com mysql

---
+++ exercises2/books.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book sistem</title>
+    <link rel="stylesheet" href="books.css">
+</head>
+<body>
+    <form method='get' >
+    <fieldset><legend>Cadastro de livros</legend>
+        <label for="titulo">Título:<input type="text" name='titulo' id='titulo' ></label>
+        <label for="ano_lancamento">Ano de lançamento: <input type="number" id='ano_lancamento' name="ano_lancamento"></label>
+        <label for="paginas">Páginas: <input type="number" id='paginas' min ='1' name='paginas'></label>
+        <label for="autorPrimeiro">Nome do autor: <input type="text" name='autorPrimeiro' id='autorPrimeiro'></label>
+        <label for="autorUltimo">Sobrenome do autor: <input type="text" name='autorUltimo' id='autorUltimo'></label><br>
+    
+        <label for="estoque">Quantidade: <input type="number" min='0' id='estoque' name='estoque'></label>
+
+        <input type="submit" value='Cadastrar' id='submit'>
+
+        <?php
+            $titulo = $_GET['titulo'];
+            $ano = $_GET['ano_lancamento'];
+            $paginas = $_GET['paginas'];
+            $autorPrimeiro = $_GET['autorPrimeiro'];
+            $autorUltimo = $_GET['autorUltimo'];
+            $estoque = $_GET['estoque'];
+
+            function connect($command){
+                $conn = new PDO("mysql:dbname=book_shop; host=localhost","root","anarquia99");
+                $stmt = $conn->prepare($command);
+                $stmt->execute();
+            }
+
+            if ((!isset($_GET['titulo'])) and (!isset($_GET['ano_lancamento'])) and (!isset($_GET['paginas'])) and (!isset($_GET['autorPrimeiro'])) and (!isset($_GET['autorUltimo'])) and (!isset($_GET['estoque']))) {
+                echo 'ok';
+                echo "<h4 style='margin-top:50px;'>Preencha corretamente os campos acima!</h4>";
+            }else {
+                connect("INSERT INTO books (title,author_fname,author_lname,pages,stock_quantity,released_year) VALUES ('$titulo','$autorPrimeiro','$autorUltimo',$paginas,$estoque,$ano)");
+                echo "<h3 style='margin-top:70px;'>Livro cadastrado.</h3>";
+
+            }
+
+
+        ?>
+        </fieldset>
+    </form>
+</body>
+</html>
