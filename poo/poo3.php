697a061e43fc008074f64088bfa437d5060508c1
Gabriel Bueno <64340912+lugabrielbueno@users.noreply.github.com>
2020-07-28 21:05:41 -0300

    class poo lutador php

--- poo/poo3.php
+++ poo/poo3.php
@@ -1,19 +1,54 @@
 <?php
 
 class Lutador {
-    public $;
+    private $nome;
     private $nacionalidade;
     private $idade;
     private $altura;
     private $peso;
-    protected $categoria;
-    protected $vitorias;
-    protected $derrotas;
-    protected $empates;
+    private $categoria;
+    private $vitorias;
+    private $derrotas;
+    private $empates;
 
-    public function __construct(){
-        
+    public function __construct($no,$na,$id,$al,$pe,$vi,$de,$em){
+        $this->nome = $no;
+        $this->nacionalidade = $na;
+        $this->idade = $id;
+        $this->altura = $al;
+        $this->peso = $pe;
+        $this->vitorias = $vi;
+        $this->derrotas = $de;
+        $this->empates = $em;
     }
+    public function apresentar($nome) {
+        echo "Lutador: ".getNome()."<br>";
+        echo "Origem: ".getNacionalidade()."<br>";
+        echo getIdade()." anos<br>";
+        echo getAltura()."m de altura<br>";
+        echo "Pesando ".getPeso()." kg<br>";
+        echo "Ganhou: ".getVitorias();
+        echo "Perdeu: ".getDerrotas();
+        echo "Empatou: ".getEmpates();   
+    }
+    public function status(){
+        echo getNome();
+        echo "É um peso ".getCategoria();
+        echo getVitorias()." vitórias<br>";
+        echo getDerrotas()." derrotas<br>";
+        echo getEmpates()." empates<br>";
+
+    }
+    public function ganharLuta(){
+        $this->vitorias += 1;
+    }
+    public function perderLuta(){
+        $this->derrotas += 1;
+    }
+    public function empatarLuta(){
+        $this->empates += 1;
+    }
+
     public function setNome($nome){
         $this->nome = $nome;
     }
@@ -40,9 +75,26 @@ class Lutador {
     }
     public function setPeso($peso){
         $this->peso = $peso;
+        setCategoria();
     }
     public function getPeso(){
         return $this->peso;
+    }
+    public function setCategoria(){
+        if ($this->peso<52.2){
+            $this->categoria = "Inválido";
+        }elseif ($this->peso<=70.3) {
+            $this->categoria = "Leve";
+        }elseif ($this->peso<=83.9) {
+            $this->categoria = "Médio";
+        }elseif ($this->peso<=120.2) {
+            $this->categoria = "Pesado";
+        }else {
+            $this->categoria = "Inválido";
+        }
+    }
+    public function getCategoria(){
+        echo $this->categoria;
     }
     public function setVitorias($vitorias){
         $this->vitorias = $vitorias;
