d5104785734ecad43336df4b0c18fbc9c0329c09
Gabriel Bueno <64340912+lugabrielbueno@users.noreply.github.com>
2020-07-29 20:44:34 -0300

    class poo php

---
+++ poo/Lutador.php
@@ -0,0 +1,122 @@
+<?php
+
+class Lutador {
+    private $nome;
+    private $nacionalidade;
+    private $idade;
+    private $altura;
+    private $peso;
+    private $categoria;
+    private $vitorias;
+    private $derrotas;
+    private $empates;
+
+    public function __construct($no,$na,$id,$al,$pe,$vi,$de,$em){
+        $this->nome = $no;
+        $this->nacionalidade = $na;
+        $this->idade = $id;
+        $this->altura = $al;
+        $this->setPeso($pe);
+        $this->vitorias = $vi;
+        $this->derrotas = $de;
+        $this->empates = $em;
+
+    }
+
+    public function ganharLuta(){
+        $this->vitorias += 1;
+    }
+    public function perderLuta(){
+        $this->derrotas += 1;
+    }
+    public function empatarLuta(){
+        $this->empates += 1;
+    }
+
+    public function setNome($nome){
+        $this->nome = $nome;
+    }
+    public function getNome(){
+        return $this->nome;
+    }
+    public function setNacionalidade($nacionalidade){
+        $this->nacionaliade = $nacionalidade;
+    }
+    public function getNacionalidade(){
+        return $this->nacionalidade;
+    }
+    public function setIdade($idade){
+        $this->idade = $idade;
+    }
+    public function getIdade(){
+        return $this->idade;
+    }
+    public function setAltura($altura){
+        $this->altura = $altura;
+    }
+    public function getAltura(){
+        return $this->altura;
+    }
+    public function setCategoria(){
+        if ($this->peso<52.2){
+            $this->categoria = "Inválido";
+        }elseif ($this->peso<=70.3) {
+            $this->categoria = "Leve";
+        }elseif ($this->peso<=83.9) {
+            $this->categoria = "Médio";
+        }elseif ($this->peso<=120.2) {
+            $this->categoria = "Pesado";
+        }else {
+            $this->categoria = "Inválido";
+        }
+    }
+    public function getCategoria(){
+        echo $this->categoria;
+    }
+    public function setPeso($peso){
+        $this->peso = $peso;
+        $this->setCategoria();
+    }
+    public function getPeso(){
+        return $this->peso;
+    }
+
+    public function setVitorias($vitorias){
+        $this->vitorias = $vitorias;
+    }
+    public function getVitorias(){
+        return $this->vitorias;
+    }
+    public function setDerrotas($derrotas){
+        $this->derrotas = $derrotas;
+    }
+    public function getDerrotas(){
+        return $this->derrotas;
+    }
+    public function setEmpates($empates){
+        $this->empates = $empates;
+    }
+    public function getEmpates(){
+        return $this->empates;
+    }
+    public function apresentar() {
+        echo "<br>CHEGOU A HORA! O Lutador ".$this->getNome().".";
+        echo "<br> Veio diretamente do(a) ".$this->getNacionalidade();
+        echo "<br> tem ".$this->getIdade()." anos";
+        echo "<br> com ".$this->getAltura()."m de altura.";
+        echo "<br> Pesando ".$this->getPeso()." kgs";
+        echo "<br> Já ganhou: ".$this->getVitorias()." vezes";
+        echo "<br> Perdeu: ".$this->getDerrotas();
+        echo "<br> E empatou: ".$this->getEmpates()." vezes";   
+    }
+    public function status(){
+        echo $this->getNome();
+        echo "É um peso ".$this->getCategoria();
+        echo $this->getVitorias()." vitórias";
+        echo $this->getDerrotas()." derrotas";
+        echo $this->getEmpates()." empates";
+
+    }
+}
+
+?>
--- poo/poo3.php
+++ poo/poo3.php
@@ -1,121 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
 
-class Lutador {
-    private $nome;
-    private $nacionalidade;
-    private $idade;
-    private $altura;
-    private $peso;
-    private $categoria;
-    private $vitorias;
-    private $derrotas;
-    private $empates;
+        require_once("Lutador.php");
 
-    public function __construct($no,$na,$id,$al,$pe,$vi,$de,$em){
-        $this->nome = $no;
-        $this->nacionalidade = $na;
-        $this->idade = $id;
-        $this->altura = $al;
-        $this->peso = $pe;
-        $this->vitorias = $vi;
-        $this->derrotas = $de;
-        $this->empates = $em;
-    }
-    public function apresentar($nome) {
-        echo "Lutador: ".getNome()."<br>";
-        echo "Origem: ".getNacionalidade()."<br>";
-        echo getIdade()." anos<br>";
-        echo getAltura()."m de altura<br>";
-        echo "Pesando ".getPeso()." kg<br>";
-        echo "Ganhou: ".getVitorias();
-        echo "Perdeu: ".getDerrotas();
-        echo "Empatou: ".getEmpates();   
-    }
-    public function status(){
-        echo getNome();
-        echo "É um peso ".getCategoria();
-        echo getVitorias()." vitórias<br>";
-        echo getDerrotas()." derrotas<br>";
-        echo getEmpates()." empates<br>";
+        $l = array();
 
-    }
-    public function ganharLuta(){
-        $this->vitorias += 1;
-    }
-    public function perderLuta(){
-        $this->derrotas += 1;
-    }
-    public function empatarLuta(){
-        $this->empates += 1;
-    }
+        $l[0] = new Lutador('Pretty Boy',"França",31,1.75,68.9,11,2,1);
+        $l[1] = new Lutador('Putscript',"Brasil",29,1.68,57.8,14,2,3);
+        $l[2] = new Lutador("Snap Shadow","EUA",35,1.65,80.9,12,2,1);
+        $l[3] = new Lutador("Dead Code","Austrália",28,1.93,81.6,13,0,2);
+        $l[4] = new Lutador("UFO Cobol","Brasil",37,1.70,119.3,5,4,3);
+        $l[5] = new Lutador("Nerd Art","EUA",30,1.81,105.7,12,2,4);
 
-    public function setNome($nome){
-        $this->nome = $nome;
-    }
-    public function getNome(){
-        return $this->nome;
-    }
-    public function setNacionalidade($nacionalidade){
-        $this->nacionaliade = $nacionalidade;
-    }
-    public function getNacionalidade(){
-        return $this->nacionalidade;
-    }
-    public function setIdade($idade){
-        $this->idade = $idade;
-    }
-    public function getIdade(){
-        return $this->idade;
-    }
-    public function setAltura($altura){
-        $this->altura = $altura;
-    }
-    public function getAltura(){
-        return $this->altura;
-    }
-    public function setPeso($peso){
-        $this->peso = $peso;
-        setCategoria();
-    }
-    public function getPeso(){
-        return $this->peso;
-    }
-    public function setCategoria(){
-        if ($this->peso<52.2){
-            $this->categoria = "Inválido";
-        }elseif ($this->peso<=70.3) {
-            $this->categoria = "Leve";
-        }elseif ($this->peso<=83.9) {
-            $this->categoria = "Médio";
-        }elseif ($this->peso<=120.2) {
-            $this->categoria = "Pesado";
-        }else {
-            $this->categoria = "Inválido";
-        }
-    }
-    public function getCategoria(){
-        echo $this->categoria;
-    }
-    public function setVitorias($vitorias){
-        $this->vitorias = $vitorias;
-    }
-    public function getVitorias(){
-        return $this->vitorias;
-    }
-    public function setDerrotas($derrotas){
-        $this->derrotas = $derrotas;
-    }
-    public function getDerrotas(){
-        return $this->derrotas;
-    }
-    public function setEmpates($empates){
-        $this->empates = $empates;
-    }
-    public function getEmpates(){
-        return $this->empates;
-    }
+        $l[1]->apresentar();
+        $l[1]->perderLuta();
+        $l[1]->apresentar();
 
-}
-
-
-?>
+    ?>
+</body>
+</html>
