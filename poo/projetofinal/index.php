fac2f62443a78f74764007f22f042509cb6b9354
Gabriel Bueno <64340912+lugabrielbueno@users.noreply.github.com>
2020-08-01 15:40:17 -0300

    poo php

--- poo/projetofinal/index.php
+++ poo/projetofinal/index.php
@@ -1,12 +1,16 @@
 <?php
 require_once("Video.php");
-
+require_once("Gafanhoto.php");
 
 $v[0] = new Video('Aula 1 de POO');
 $v[1] = new Video('Aula 12 de HTML');
 $v[2] = new Video('Aula 15 de PHP');
 
-var_dump($v);
+$g[0] = new Gafanhoto('Gabriel',25,'M','GabriBueno');
+$g[1] = new Gafanhoto('Flavia',24,'F','Flavialise');
+
+
+var_dump($v,$g);
 
 
 
